ae3c60614762cde1823e118ff6402687f28fa1cb
Kuba Marek <blue.cube@seznam.cz>
2023-12-01 18:34:27 +0100

    Zippity WIP -- can write archive with empty files

--- zippity/src/lib.rs
+++ zippity/src/lib.rs
@@ -1,9 +1,11 @@
 use lru::LruCache;
-use packed_struct::{PackedStructInfo, PackedStructSlice};
+use packed_struct::{PackedStruct, PackedStructSlice};
 use pin_project::pin_project;
+use std::collections::BTreeMap;
 use std::num::NonZeroUsize;
 use std::pin::Pin;
 use std::task::{ready, Context, Poll};
+use structs::PackedStructZippityExt;
 use tokio::{
     io::{AsyncRead, AsyncSeek, ReadBuf},
     sync::Mutex,
@@ -12,6 +14,9 @@ use vecbuffer::VecBuffer;
 
 mod structs;
 mod vecbuffer;
+
+/// Minimum version needed to extract the zip64 extensions required by zippity
+pub const ZIP64_VERSION_TO_EXTRACT: u16 = 45;
 
 pub trait EntryData {
     type Reader: AsyncRead;
@@ -64,22 +69,23 @@ where
 
 #[derive(Clone, Debug)]
 struct BuilderEntry<D> {
-    name: String,
     data: D,
 }
 
 impl<D: EntryData> BuilderEntry<D> {
-    fn get_size(&self) -> u64 {
-        let local_header = (structs::LocalFileHeader::packed_bits() as u64) / 8;
-        let filename = self.name.len() as u64;
+    fn get_local_size(&self, name: &str) -> u64 {
+        let local_header = structs::LocalFileHeader::packed_size();
+        let filename = name.len() as u64;
         let data = self.data.get_size();
-        let data_descriptor = (structs::DataDescriptor64::packed_bits() as u64) / 8;
+        //let data_descriptor = structs::DataDescriptor64::packed_size();
 
-        local_header + filename + data + data_descriptor
+        local_header + filename + data
     }
 
-    fn get_cd_entry_size(&self) -> u64 {
-        todo!()
+    fn get_cd_header_size(&self, name: &str) -> u64 {
+        let filename = name.len() as u64;
+        let cd_entry = structs::CentralDirectoryHeader::packed_size();
+        cd_entry + filename
     }
 }
 
@@ -93,58 +99,50 @@ struct ReaderEntry<D> {
 
 #[derive(Clone, Debug)]
 pub struct Builder<D: EntryData> {
-    entries: Vec<BuilderEntry<D>>,
+    entries: BTreeMap<String, BuilderEntry<D>>,
 }
 
 impl<D: EntryData> Builder<D> {
     pub fn new() -> Self {
         Builder {
-            entries: Vec::new(),
+            entries: BTreeMap::new(),
         }
     }
 
     pub fn add_entry<T: Into<D>>(&mut self, name: String, data: T) {
         let data = data.into();
-        self.entries.push(BuilderEntry {
-            name,
-            data: data.into(),
-        });
-    }
-
-    fn get_cd_size(&self) -> u64 {
-        //let entries = self.entries.iter().map(|e| e.get_cd_entry_size()).sum();
-        //let zip64_eocd = 0;
-        //let zip64_eocd_locator = 0;
-
-        0
+        self.entries
+            .insert(name, BuilderEntry { data: data.into() });
     }
 
     pub fn build<'a>(self, crc_cache: &'a CrcCache) -> Reader<'a, D> {
-        let cd_size = self.get_cd_size();
+        let mut offset: u64 = 0;
+        let mut cd_size: u64 = 0;
         let entries: Vec<_> = self
             .entries
             .into_iter()
-            .scan(0, |accumulator, entry| {
-                let size = entry.data.get_size() + (entry.name.len() as u64) + 50; // TODO: Fix the structures size!
-                let offset = *accumulator;
-                *accumulator += size;
-                Some(ReaderEntry {
-                    name: entry.name,
+            .map(|(name, entry)| {
+                let size = entry.get_local_size(&name);
+                let offset_copy = offset;
+                offset += size;
+                cd_size += entry.get_cd_header_size(&name);
+                ReaderEntry {
+                    name,
                     data: entry.data,
                     size,
-                    offset,
-                })
+                    offset: offset_copy,
+                }
             })
             .collect();
 
-        let total_size = entries.last().map_or(0, |entry| entry.offset + entry.size)
-            + cd_size
-            + (structs::EndOfCentralDirectory::packed_bytes_size(None).unwrap() as u64);
+        let cd_offset = offset;
+        let total_size = cd_offset + cd_size + structs::EndOfCentralDirectory::packed_size();
         let state = ZipPiece::new(&entries);
 
         Reader {
             entries,
 
+            cd_offset,
             cd_size,
             total_size,
 
@@ -159,9 +157,17 @@ impl<D: EntryData> Builder<D> {
 #[derive(Debug)]
 enum ZipPiece {
     LocalHeader(usize),
+    LocalHeaderFilename {
+        entry_index: usize,
+        bytes_written: usize,
+    },
     FileData(usize), // TODO: Hasher
     DataDescriptor(usize),
     CDFileHeader(usize),
+    CDFileHeaderFilename {
+        entry_index: usize,
+        bytes_written: usize,
+    },
     CDEnd,
     Finished,
 }
@@ -180,6 +186,8 @@ impl ZipPiece {
 pub struct Reader<'a, D: EntryData> {
     /// Vector of entries and their offsets  (counted from start of file)
     entries: Vec<ReaderEntry<D>>,
+
+    cd_offset: u64,
     cd_size: u64,
     total_size: u64,
 
@@ -194,7 +202,49 @@ impl<'a, D: EntryData> Reader<'a, D> {
         self.total_size
     }
 
-    fn make_eocd(&self) -> impl PackedStructSlice {
+    fn make_local_header(&self, entry_index: usize) -> impl PackedStruct {
+        structs::LocalFileHeader {
+            signature: structs::LocalFileHeader::SIGNATURE,
+            version_to_extract: ZIP64_VERSION_TO_EXTRACT,
+            flags: 0,
+            compression: structs::Compression::Store,
+            last_mod_time: 0,
+            last_mod_date: 0,
+            crc32: 0,
+            compressed_size: 0,
+            uncompressed_size: 0,
+            file_name_len: self.entries[entry_index].name.len() as u16,
+            extra_field_len: 0,
+        }
+    }
+
+    fn make_cd_header(&self, entry_index: usize) -> impl PackedStruct {
+        let entry = &self.entries[entry_index];
+        structs::CentralDirectoryHeader {
+            signature: structs::CentralDirectoryHeader::SIGNATURE,
+            version_made_by: structs::VersionMadeBy {
+                os: structs::VersionMadeByOs::UNIX,
+                spec_version: ZIP64_VERSION_TO_EXTRACT as u8,
+            },
+            version_to_extract: ZIP64_VERSION_TO_EXTRACT,
+            flags: 0,
+            compression: structs::Compression::Store,
+            last_mod_time: 0,
+            last_mod_date: 0,
+            crc32: 0,
+            compressed_size: 0,
+            uncompressed_size: 0,
+            file_name_len: entry.name.len() as u16,
+            extra_field_len: 0,
+            file_comment_length: 0,
+            disk_number_start: 0,
+            internal_attributes: 0,
+            external_attributes: 0,
+            local_header_offset: entry.offset as u32,
+        }
+    }
+
+    fn make_eocd(&self) -> impl PackedStruct {
         structs::EndOfCentralDirectory {
             signature: structs::EndOfCentralDirectory::SIGNATURE,
             this_disk_number: 0,
@@ -202,19 +252,19 @@ impl<'a, D: EntryData> Reader<'a, D> {
             this_cd_entry_count: self.entries.len() as u16,
             total_cd_entry_count: self.entries.len() as u16,
             size_of_cd: self.cd_size as u32,
-            offset_of_cd_start: 0,
+            cd_offset: self.cd_offset as u32,
             file_comment_length: 0,
         }
     }
 
-    fn write_packed_struct(
+    fn write_packed_struct<T: PackedStruct>(
         mut self: Pin<&mut Self>,
-        ps: impl PackedStructSlice,
+        ps: T,
         output: &mut ReadBuf<'_>,
     ) {
         assert!(!self.buffer.has_remaining());
 
-        let size = PackedStructSlice::packed_bytes_size(Some(&ps)).unwrap();
+        let size = T::packed_size() as usize;
 
         if output.remaining() >= size {
             let buf_slice = output.initialize_unfilled_to(size);
@@ -227,6 +277,25 @@ impl<'a, D: EntryData> Reader<'a, D> {
             self.buffer.read_into_readbuf(output);
         }
     }
+
+    fn write_filename(
+        &self,
+        entry_index: usize,
+        bytes_written: usize,
+        output: &mut ReadBuf<'_>,
+    ) -> Option<usize> {
+        let bytes = self.entries[entry_index].name.as_bytes();
+        let bytes = &bytes[bytes_written..];
+
+        if bytes.len() <= output.remaining() {
+            output.put_slice(bytes);
+            None
+        } else {
+            let new_bytes_written = bytes_written + output.remaining();
+            output.put_slice(&bytes[..output.remaining()]);
+            Some(new_bytes_written)
+        }
+    }
 }
 
 impl<'a, D: EntryData> AsyncRead for Reader<'a, D> {
@@ -235,6 +304,7 @@ impl<'a, D: EntryData> AsyncRead for Reader<'a, D> {
         ctx: &mut Context<'_>,
         buf: &mut ReadBuf<'_>,
     ) -> Poll<std::io::Result<()>> {
+        // TODO: Don't return immediately after every write, keep looping
         loop {
             // If there is a part of a struct in the buffer waiting to be written,
             // handle that first
@@ -244,11 +314,66 @@ impl<'a, D: EntryData> AsyncRead for Reader<'a, D> {
             }
 
             match self.state {
-                /*ZipPiece::LocalHeader(entry_index) => (),
+                ZipPiece::LocalHeader(entry_index) => {
+                    let lh = self.make_local_header(entry_index);
+                    self.as_mut().write_packed_struct(lh, buf);
+                    self.state = ZipPiece::LocalHeaderFilename {
+                        entry_index,
+                        bytes_written: 0,
+                    };
+                }
+                ZipPiece::LocalHeaderFilename {
+                    entry_index,
+                    bytes_written,
+                } => {
+                    if let Some(new_bytes_written) =
+                        self.write_filename(entry_index, bytes_written, buf)
+                    {
+                        self.state = ZipPiece::LocalHeaderFilename {
+                            entry_index,
+                            bytes_written: new_bytes_written,
+                        };
+                        return Poll::Ready(Ok(()));
+                    } else {
+                        let entry_index = entry_index + 1;
+                        self.state = if entry_index < self.entries.len() {
+                            ZipPiece::LocalHeader(entry_index)
+                        } else {
+                            ZipPiece::CDFileHeader(0)
+                        };
+                    }
+                }
                 ZipPiece::FileData(entry_index) => (),
-                ZipPiece::DataDescriptor(entry_index) => Some(ZipPiece::CDFileHeader(0)),
-                ZipPiece::CDFileHeader(_) => Some(ZipPiece::CDEnd),
-                */
+                ZipPiece::DataDescriptor(entry_index) => (),
+                ZipPiece::CDFileHeader(entry_index) => {
+                    let lh = self.make_cd_header(entry_index);
+                    self.as_mut().write_packed_struct(lh, buf);
+                    self.state = ZipPiece::CDFileHeaderFilename {
+                        entry_index,
+                        bytes_written: 0,
+                    };
+                }
+                ZipPiece::CDFileHeaderFilename {
+                    entry_index,
+                    bytes_written,
+                } => {
+                    if let Some(new_bytes_written) =
+                        self.write_filename(entry_index, bytes_written, buf)
+                    {
+                        self.state = ZipPiece::CDFileHeaderFilename {
+                            entry_index,
+                            bytes_written: new_bytes_written,
+                        };
+                        return Poll::Ready(Ok(()));
+                    } else {
+                        let entry_index = entry_index + 1;
+                        self.state = if entry_index < self.entries.len() {
+                            ZipPiece::CDFileHeader(entry_index)
+                        } else {
+                            ZipPiece::CDEnd
+                        };
+                    }
+                }
                 ZipPiece::CDEnd => {
                     let eocd = self.make_eocd();
                     self.as_mut().write_packed_struct(eocd, buf);
@@ -275,8 +400,8 @@ impl<'a, D: EntryData> AsyncSeek for ArchiveReader<'a, D> {
 mod test {
     use super::*;
     use assert2::assert;
-    use proptest::prop_assume;
-    use std::future::Future;
+    use proptest::strategy::{Just, Strategy};
+    use std::{collections::HashMap, future::Future};
     use test_strategy::proptest;
     use tokio::io::AsyncReadExt;
     use zip::read::ZipArchive;
@@ -308,6 +433,16 @@ mod test {
             .block_on(fut)
     }
 
+    fn content_strategy() -> impl Strategy<Value = HashMap<String, ()>> {
+        proptest::collection::hash_map(
+            // We're limiting the character set significantly, because the zip crate we use for verification
+            // does not handle unicode filenames well.
+            r"[a-zA-Z0-91235678!@#$%^&U*/><\\\[\]]{1,1000}",
+            Just(()),
+            0..100,
+        )
+    }
+
     #[proptest]
     fn empty(#[strategy(1usize..8192usize)] read_size: usize) {
         let cache = CrcCache::unbounded();
@@ -320,5 +455,33 @@ mod test {
 
         let unpacked = ZipArchive::new(std::io::Cursor::new(buf)).expect("Should be a valid zip");
         assert!(unpacked.is_empty());
+    }
+
+    #[proptest]
+    fn alltest(
+        #[strategy(content_strategy())] content: HashMap<String, ()>,
+        #[strategy(1usize..8192usize)] read_size: usize,
+    ) {
+        let mut builder = Builder::new();
+
+        content.iter().for_each(|(name, _value)| {
+            builder.add_entry(name.clone(), ());
+        });
+
+        let cache = CrcCache::unbounded();
+        let zippity: Reader<()> = builder.build(&cache);
+        let size = zippity.get_size();
+
+        let buf = unasync(read_to_vec(zippity, read_size));
+
+        assert!(size == (buf.len() as u64));
+
+        let unpacked = ZipArchive::new(std::io::Cursor::new(buf)).expect("Should be a valid zip");
+        let unpacked_names = unpacked
+            .file_names()
+            .map(|name| (name.to_string(), ()))
+            .collect::<HashMap<_, _>>();
+
+        assert!(unpacked_names == content);
     }
 }
--- zippity/src/structs.rs
+++ zippity/src/structs.rs
@@ -1,8 +1,5 @@
 use packed_struct::prelude::*;
 
-/// Minimum version needed to extract the zip64 extensions required by zippity
-const ZIP64_VERSION_TO_EXTRACT: u16 = 45;
-
 /// Local file header
 /// Preceedes every file.
 /// Must be followed by file name nad extra fields (length is part of this struct)
@@ -12,7 +9,8 @@ pub struct LocalFileHeader {
     pub signature: u32,
     pub version_to_extract: u16,
     pub flags: u16,
-    pub compression: u16,
+    #[packed_field(size_bytes = "2", ty = "enum")]
+    pub compression: Compression,
     pub last_mod_time: u16,
     pub last_mod_date: u16,
     pub crc32: u32,
@@ -23,7 +21,7 @@ pub struct LocalFileHeader {
 }
 
 impl LocalFileHeader {
-    const LOCAL_FILE_HEADER_SIGNATURE: u32 = 0x04034b50;
+    pub const SIGNATURE: u32 = 0x04034b50;
 }
 
 #[derive(Debug, PackedStruct)]
@@ -49,7 +47,43 @@ pub struct DataDescriptor64 {
 #[derive(Debug, PackedStruct)]
 #[packed_struct(endian = "lsb")]
 pub struct CentralDirectoryHeader {
-    pub TODO: u8,
+    pub signature: u32,
+    #[packed_field(size_bytes = "2")]
+    pub version_made_by: VersionMadeBy,
+    pub version_to_extract: u16,
+    pub flags: u16,
+    #[packed_field(size_bytes = "2", ty = "enum")]
+    pub compression: Compression,
+    pub last_mod_time: u16,
+    pub last_mod_date: u16,
+    pub crc32: u32,
+    pub compressed_size: u32,
+    pub uncompressed_size: u32,
+    pub file_name_len: u16,
+    pub extra_field_len: u16,
+    pub file_comment_length: u16,
+    pub disk_number_start: u16,
+    pub internal_attributes: u16,
+    pub external_attributes: u32,
+    pub local_header_offset: u32,
+}
+
+impl CentralDirectoryHeader {
+    pub const SIGNATURE: u32 = 0x02014b50;
+}
+
+#[derive(Debug, PackedStruct)]
+#[packed_struct(endian = "lsb")]
+pub struct VersionMadeBy {
+    #[packed_field(size_bytes = "1", ty = "enum")]
+    pub os: VersionMadeByOs,
+    pub spec_version: u8,
+}
+
+#[derive(Clone, Copy, Debug, PrimitiveEnum_u8)]
+#[non_exhaustive]
+pub enum VersionMadeByOs {
+    UNIX = 3,
 }
 
 #[derive(Debug, PackedStruct)]
@@ -61,10 +95,26 @@ pub struct EndOfCentralDirectory {
     pub this_cd_entry_count: u16,
     pub total_cd_entry_count: u16,
     pub size_of_cd: u32,
-    pub offset_of_cd_start: u32,
+    pub cd_offset: u32,
     pub file_comment_length: u16,
 }
 
 impl EndOfCentralDirectory {
     pub const SIGNATURE: u32 = 0x06054b50;
+}
+
+#[derive(Clone, Copy, Debug, PrimitiveEnum_u16)]
+#[non_exhaustive]
+pub enum Compression {
+    Store = 1,
+}
+
+pub trait PackedStructZippityExt {
+    fn packed_size() -> u64;
+}
+
+impl<T: PackedStruct> PackedStructZippityExt for T {
+    fn packed_size() -> u64 {
+        Self::packed_bytes_size(None).unwrap() as u64
+    }
 }
