b1c94719bd125a5b1eba46294a822a5577cf7095
Kuba Marek <blue.cube@seznam.cz>
2024-03-22 15:16:44 +0100

    Start of the refactoring to multiple files

---
+++ src/entry_data.rs
@@ -0,0 +1,38 @@
+use std::future::Future;
+use std::io::Result;
+
+use tokio::io::AsyncRead;
+
+pub trait EntryData {
+    type Reader: AsyncRead;
+    type ReaderFuture: Future<Output = Result<Self::Reader>>;
+
+    fn size(&self) -> u64;
+    fn get_reader(&self) -> Self::ReaderFuture;
+}
+
+impl EntryData for () {
+    type Reader = std::io::Cursor<&'static [u8]>;
+    type ReaderFuture = std::future::Ready<Result<Self::Reader>>;
+
+    fn size(&self) -> u64 {
+        0
+    }
+
+    fn get_reader(&self) -> Self::ReaderFuture {
+        std::future::ready(Ok(std::io::Cursor::new(&[])))
+    }
+}
+
+impl<'a> EntryData for &'a [u8] {
+    type Reader = std::io::Cursor<&'a [u8]>;
+    type ReaderFuture = std::future::Ready<Result<Self::Reader>>;
+
+    fn size(&self) -> u64 {
+        self.len() as u64
+    }
+
+    fn get_reader(&self) -> Self::ReaderFuture {
+        std::future::ready(Ok(std::io::Cursor::new(self)))
+    }
+}
---
+++ src/zippity_error.rs
@@ -0,0 +1,15 @@
+use thiserror::Error;
+
+#[derive(Clone, Debug, Error, PartialEq)]
+pub enum ZippityError {
+    #[error("Entry name too long (length must fit into 16bit)")]
+    TooLongEntryName { entry_name: String },
+    #[error("Entry {entry_name} reports length {expected_size} B, but was {actual_size} B")]
+    LengthMismatch {
+        entry_name: String,
+        expected_size: u64,
+        actual_size: u64,
+    },
+    #[error("Attempting to seek outside of the file")]
+    SeekingOutsideOfFile,
+}
