29474a8873f18809f15d5b0a14475ee96b588894
Kuba Marek <blue.cube@seznam.cz>
2025-02-19 22:48:56 +0100

    Setting datetime from system time and chrono

--- README.md
+++ README.md
@@ -64,3 +64,4 @@ The library is mostly finished, with lot of polish missing for version 1.0.0.
 | `bytes` | Implement `EntryData` for `bytes::Bytes`, and provide method `into_bytes_stream()` for `Reader`. | no |
 | `actix-web` | Adds`actix_web::Reponder` implementation to `zippity::Reader` | no |
 | `proptest` | Add module `zippity::proptest`, with strategies, and `proptest::arbitrary::Arbitrary` implementation for `Reader`. | no |
+| `chrono` | Support for setting last modification date and time metadata using types from `chrono`. | false |
--- src/builder.rs
+++ src/builder.rs
@@ -1,3 +1,7 @@
+use std::{fmt::Debug, rc::Rc, time::SystemTime};
+
+#[cfg(feature = "chrono")]
+use chrono::{Datelike, NaiveDateTime, TimeZone, Timelike};
 use indexmap::IndexMap;
 
 use crate::{
@@ -7,13 +11,28 @@ use crate::{
     Error,
 };
 
-#[derive(Clone, Debug)]
+type TimeConverter = Rc<dyn Fn(SystemTime) -> (i32, u32, u32, u32, u32, u32)>;
+
+#[derive(Clone)]
 pub struct BuilderEntry<D> {
     data: D,
     crc32: Option<u32>,
     datetime: Option<structs::DosDatetime>,
     file_type: structs::unix_mode::FileType,
     permissions: Option<u32>,
+    time_converter: Option<TimeConverter>,
+}
+
+impl<D: EntryData + Debug> Debug for BuilderEntry<D> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("BuilderEntry")
+            .field("data", &self.data)
+            .field("crc32", &self.crc32)
+            .field("datetime", &self.datetime)
+            .field("file_type", &self.file_type)
+            .field("permissions", &self.permissions)
+            .finish_non_exhaustive()
+    }
 }
 
 impl<D: EntryData> BuilderEntry<D> {
@@ -32,7 +51,9 @@ impl<D: EntryData> BuilderEntry<D> {
     }
 
     /// Sets the last modification date and time of the entry.
+    ///
     /// Returns None if the date is out of the representable range (1980-1-1 to 2107-12-31)
+    ///
     /// Note that only even seconds can be stored and the value will get rounded down.
     pub fn datetime_fields(
         &mut self,
@@ -50,9 +71,13 @@ impl<D: EntryData> BuilderEntry<D> {
     }
 
     /// Sets the last modification date and time of the entry.
+    ///
     /// If the date is out of the representable range (1980-1-1 to 2107-12-31), this method
-    /// ignores the error and continues a default value (1980-1-1T00:00:00).
+    /// ignores the error and keeps previous value (default value is 1980-1-1T00:00:00).
+    ///
     /// Note that only even seconds can be stored and the value will get rounded down.
+    ///
+    /// This is equivalent to calling `Builder::datetime_fields`, but ignoring the possible `None`.
     pub fn datetime_fields_or_default(
         &mut self,
         year: i32,
@@ -66,6 +91,81 @@ impl<D: EntryData> BuilderEntry<D> {
         self
     }
 
+    /// Sets the last modification date and time of the entry.
+    ///
+    /// Uses the time converter set by the last call to `Builder::system_time_converter`, or `Builder::system_timezone`.
+    ///
+    /// Returns None if the converter is not set, or date is out of the representable range (1980-1-1 to 2107-12-31)
+    /// Note that only even seconds can be stored and the value will get rounded down.
+    ///
+    /// Note that only even seconds can be stored and the value will get rounded down.
+    ///
+    /// This is equivalent to calling `Builder::datetime_fields`, with `SystemTime` converted to fields representation.
+    pub fn datetime_system(&mut self, datetime: SystemTime) -> Option<&mut Self> {
+        let converter = self.time_converter.as_ref()?;
+        let converted = converter(datetime);
+        self.datetime_fields(
+            converted.0,
+            converted.1,
+            converted.2,
+            converted.3,
+            converted.4,
+            converted.5,
+        )
+    }
+
+    /// Sets the last modification date and time of the entry.
+    ///
+    /// Uses the time converter set by the last call to `Builder::system_time_converter`, or `Builder::system_timezone`.
+    ///
+    /// If the converter is not set, or the date is out of the representable range (1980-1-1 to 2107-12-31),
+    /// this method ignores the error and keeps previous value (default value is 1980-1-1T00:00:00).
+    ///
+    /// Note that only even seconds can be stored and the value will get rounded down.
+    ///
+    /// This is equivalent to calling `Builder::datetime_fields_or_default`, with `SystemTime` converted to fields representation.
+    pub fn datetime_system_or_default(&mut self, datetime: SystemTime) -> &mut Self {
+        self.datetime_system(datetime);
+        self
+    }
+
+    #[cfg(feature = "chrono")]
+    /// Sets the last modification date and time of the entry.
+    ///
+    /// Returns None if the date is out of the representable range (1980-1-1 to 2107-12-31)
+    ///
+    /// Note that only even seconds can be stored and the value will get rounded down.
+    pub fn datetime(&mut self, datetime: NaiveDateTime) -> Option<&mut Self> {
+        self.datetime_fields(
+            datetime.year(),
+            datetime.month(),
+            datetime.day(),
+            datetime.hour(),
+            datetime.minute(),
+            datetime.second(),
+        )
+    }
+
+    #[cfg(feature = "chrono")]
+    /// Sets the last modification date and time of the entry.
+    ///
+    /// If the date is out of the representable range (1980-1-1 to 2107-12-31), this method
+    /// ignores the error and keeps previous value (default value is 1980-1-1T00:00:00).
+    ///
+    /// Note that only even seconds can be stored and the value will get rounded down.
+    ///
+    /// This is equivalent to calling `Builder::datetime_fields`, but ignoring the possible `None`.
+    pub fn datetime_or_default(&mut self, datetime: NaiveDateTime) -> &mut Self {
+        self.datetime_fields(
+            datetime.year(),
+            datetime.month(),
+            datetime.day(),
+            datetime.hour(),
+            datetime.minute(),
+            datetime.second(),
+        );
+        self
+    }
     /// Sets the entry to be a directory.
     /// This is the default.
     pub fn file(&mut self) -> &mut Self {
@@ -115,10 +215,26 @@ impl<D: EntryData> BuilderEntry<D> {
     }
 }
 
-#[derive(Clone, Debug)]
 /// Represents entries of the zip file, which can be converted to a `Reader`.
+#[derive(Clone)]
 pub struct Builder<D: EntryData> {
-    entries: IndexMap<String, BuilderEntry<D>>,
+    pub entries: IndexMap<String, BuilderEntry<D>>,
+    time_converter: Option<TimeConverter>,
+}
+
+impl<D: EntryData + Debug> Debug for Builder<D> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Builder")
+            .field("entries", &self.entries)
+            .field(
+                "time_converter",
+                match self.time_converter {
+                    Some(_) => &"Some(_)",
+                    None => &"None",
+                },
+            )
+            .finish()
+    }
 }
 
 impl<D: EntryData> Default for Builder<D> {
@@ -132,6 +248,7 @@ impl<D: EntryData> Builder<D> {
     pub fn new() -> Self {
         Builder {
             entries: IndexMap::new(),
+            time_converter: None,
         }
     }
 
@@ -166,6 +283,7 @@ impl<D: EntryData> Builder<D> {
             datetime: None,
             file_type: structs::unix_mode::FileType::File,
             permissions: None,
+            time_converter: self.time_converter.clone(),
         });
         Ok(inserted)
     }
@@ -214,6 +332,48 @@ impl<D: EntryData> Builder<D> {
             entries,
         )
     }
+
+    /// Sets a converter function that will be used for converting system times to field representation.
+    ///
+    /// The converter is passed to newly created builder entries and used in `BuilderEntry::datetime_system` and
+    /// `BuilderEntry::datetime_system_or_default`.
+    pub fn system_time_converter<F>(&mut self, converter: F) -> &mut Self
+    where
+        F: Fn(SystemTime) -> (i32, u32, u32, u32, u32, u32) + 'static,
+    {
+        self.time_converter = Some(Rc::new(converter));
+        self
+    }
+
+    /// Sets a converter function based on a `chrono::TimeZone`.
+    ///
+    /// This is a wrapper over `system_time_converter`.
+    ///
+    /// The converter is passed to newly created builder entries and used in `BuilderEntry::datetime_system`,
+    /// `BuilderEntry::datetime_system_or_default` and `BuilderEntry::metadata`.
+    #[cfg(feature = "chrono")]
+    pub fn system_time_timezone<Tz>(&mut self, tz: Tz) -> &mut Self
+    where
+        Tz: TimeZone + 'static,
+    {
+        use chrono::{DateTime, Utc};
+
+        self.time_converter = Some(Rc::new(move |system_time| {
+            let naive = DateTime::<Utc>::from(system_time)
+                .with_timezone(&tz)
+                .naive_local();
+            (
+                naive.year(),
+                naive.month(),
+                naive.day(),
+                naive.hour(),
+                naive.minute(),
+                naive.second(),
+            )
+        }));
+
+        self
+    }
 }
 
 #[cfg(test)]
@@ -224,9 +384,17 @@ mod test {
     use assert2::assert;
     use assert_matches::assert_matches;
     use bytes::Bytes;
+    use structs::DosDatetime;
 
-    use std::collections::HashMap;
+    use std::{collections::HashMap, time::UNIX_EPOCH};
 
+    #[cfg(feature = "chrono")]
+    use proptest::prop_assume;
+
+    #[cfg(feature = "chrono")]
+    use chrono::FixedOffset;
+
+    use test_case::test_matrix;
     use test_strategy::proptest;
 
     #[tokio::test]
@@ -281,15 +449,164 @@ mod test {
         }
     }
 
+    fn datetime_fields(entry: &mut BuilderEntry<()>, year: i32, is_some: bool) {
+        assert!(entry.datetime_fields(year, 1, 1, 1, 1, 1).is_some() == is_some);
+    }
+
+    fn datetime_fields_or_default(entry: &mut BuilderEntry<()>, year: i32, _is_some: bool) {
+        entry.datetime_fields_or_default(year, 1, 1, 1, 1, 1);
+    }
+
+    #[cfg(feature = "chrono")]
+    fn datetime(entry: &mut BuilderEntry<()>, year: i32, is_some: bool) {
+        let chrono_datetime = chrono::NaiveDate::from_ymd_opt(year, 1, 1)
+            .unwrap()
+            .and_hms_opt(1, 1, 1)
+            .unwrap();
+        assert!(entry.datetime(chrono_datetime).is_some() == is_some);
+    }
+
+    #[cfg(feature = "chrono")]
+    fn datetime_or_default(entry: &mut BuilderEntry<()>, year: i32, _is_some: bool) {
+        let chrono_datetime = chrono::NaiveDate::from_ymd_opt(year, 1, 1)
+            .unwrap()
+            .and_hms_opt(1, 1, 1)
+            .unwrap();
+        entry.datetime_or_default(chrono_datetime);
+    }
+
+    fn datetime_system(entry: &mut BuilderEntry<()>, year: i32, is_some: bool) {
+        entry.time_converter = Some(Rc::new(move |_| (year, 1, 1, 1, 1, 1)));
+        assert!(entry.datetime_system(UNIX_EPOCH).is_some() == is_some);
+    }
+
+    fn datetime_system_or_default(entry: &mut BuilderEntry<()>, year: i32, _is_some: bool) {
+        entry.time_converter = Some(Rc::new(move |_| (year, 1, 1, 1, 1, 1)));
+        entry.datetime_system_or_default(UNIX_EPOCH);
+    }
+
+    #[cfg_attr(feature = "chrono", test_matrix(
+        [
+            datetime_fields,
+            datetime_fields_or_default,
+            datetime,
+            datetime_or_default,
+            datetime_system,
+            datetime_system_or_default,
+        ],
+        [2024, 1000]
+    ))]
+    #[cfg_attr(not(feature = "chrono"), test_matrix(
+        [
+            datetime_fields,
+            datetime_fields_or_default,
+            datetime_system,
+            datetime_system_or_default,
+        ],
+        [2024, 1000]
+    ))]
+    fn datetime_test(setter_fn: impl FnOnce(&mut BuilderEntry<()>, i32, bool), year: i32) {
+        let is_some = year >= 1980;
+
+        let mut entry = BuilderEntry {
+            data: (),
+            crc32: None,
+            datetime: None,
+            file_type: structs::unix_mode::FileType::File,
+            permissions: None,
+            time_converter: None,
+        };
+
+        setter_fn(&mut entry, year, is_some);
+
+        assert!(entry.datetime.is_some() == is_some);
+        if let Some(dos_datetime) = entry.datetime {
+            assert!(dos_datetime == DosDatetime::new(year, 1, 1, 1, 1, 1).unwrap());
+        }
+    }
+
+    #[cfg(feature = "chrono")]
+    #[proptest]
+    fn datetime_chrono_matches(
+        #[strategy(1900..2100)] year: i32,
+        #[strategy(1u32..=12u32)] month: u32,
+        #[strategy(1u32..=31u32)] day: u32,
+        #[strategy(0u32..24u32)] hour: u32,
+        #[strategy(0u32..60u32)] minute: u32,
+        #[strategy(0u32..60u32)] second: u32,
+    ) {
+        let Some(chrono_datetime) = chrono::NaiveDate::from_ymd_opt(year, month, day)
+            .and_then(|d| d.and_hms_opt(hour, minute, second))
+        else {
+            prop_assume!(false);
+            unreachable!();
+        };
+
+        let mut entry1 = BuilderEntry {
+            data: (),
+            crc32: None,
+            datetime: None,
+            file_type: structs::unix_mode::FileType::File,
+            permissions: None,
+            time_converter: None,
+        };
+        let mut entry2 = entry1.clone();
+
+        entry1.datetime_fields(year, month, day, hour, minute, second);
+        entry2.datetime(chrono_datetime);
+
+        assert!(entry1.datetime == entry2.datetime);
+    }
+
     #[test]
-    fn datetime_default_works() {
-        let mut builder = Builder::<()>::new();
-        builder
+    fn system_time_conversion_comes_from_builder() {
+        let mut builder: Builder<()> = Builder::new();
+        builder.system_time_converter(|_| (2000, 1, 2, 3, 4, 5));
+
+        let x: &BuilderEntry<()> = builder
             .add_entry("X".into(), ())
             .unwrap()
-            .datetime_fields_or_default(3000, 1, 1, 1, 1, 1);
+            .datetime_system(SystemTime::UNIX_EPOCH)
+            .unwrap();
+        let mut y = BuilderEntry {
+            data: (),
+            crc32: None,
+            datetime: None,
+            file_type: structs::unix_mode::FileType::File,
+            permissions: None,
+            time_converter: None,
+        };
+        y.datetime_fields(2000, 1, 2, 3, 4, 5).unwrap();
 
-        assert!(builder.entries["X"].datetime.is_none());
+        assert!(x.datetime == y.datetime);
+    }
+
+    #[cfg(feature = "chrono")]
+    #[proptest]
+    fn system_time_timezeone(#[strategy(-5..=5)] offset: i32) {
+        let timezone = FixedOffset::east_opt(offset).unwrap();
+
+        let mut builder: Builder<()> = Builder::new();
+
+        builder.system_time_timezone(timezone);
+
+        let chrono_datetime = chrono::NaiveDate::from_ymd_opt(2025, 02, 02)
+            .unwrap()
+            .and_hms_opt(17, 0, 0)
+            .unwrap()
+            .and_utc();
+        let system_datetime: SystemTime = chrono_datetime.into();
+        let chrono_converted = chrono_datetime.with_timezone(&timezone).naive_local();
+
+        let converter = builder.time_converter.unwrap();
+        let converted = converter(system_datetime);
+
+        assert!(converted.0 == chrono_converted.year());
+        assert!(converted.1 == chrono_converted.month());
+        assert!(converted.2 == chrono_converted.day());
+        assert!(converted.3 == chrono_converted.hour());
+        assert!(converted.4 == chrono_converted.minute());
+        assert!(converted.5 == chrono_converted.second());
     }
 
     #[test]
@@ -300,6 +617,7 @@ mod test {
             datetime: None,
             file_type: structs::unix_mode::FileType::File,
             permissions: None,
+            time_converter: None,
         };
         entry.unix_permissions(0o123456);
 
